283bbdc3011648324854bf3ea46b7aa03f386587
stuncloud <stuncloud+github@gmail.com>
2024-03-05 14:27:18 +0900

    :up: export RawWindowHandle and GetRawWindowHandle

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 
 #[cfg(all(not(feature = "baseview"), feature = "winit"))]
 pub use vizia_winit::application::Application;
+pub use vizia_winit::rwh::RawWindowHandle;
+pub use vizia_winit::GetRawWindowHandle;
 
 #[cfg(all(not(feature = "winit"), feature = "baseview"))]
 pub use vizia_baseview::{Application, ParentWindow, WindowScalePolicy};
@@ -14,6 +16,8 @@ pub mod prelude {
 
     #[cfg(all(not(feature = "baseview"), feature = "winit"))]
     pub use vizia_winit::application::Application;
+    pub use vizia_winit::rwh::RawWindowHandle;
+    pub use vizia_winit::GetRawWindowHandle;
 
     #[cfg(all(not(feature = "winit"), feature = "baseview"))]
     pub use vizia_baseview::Application;
